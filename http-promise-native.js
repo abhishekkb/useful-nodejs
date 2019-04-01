4fff7ffd568c3e281c3e45f972f4f8180ddbc028
abhishekkb <abhishekkb@users.noreply.github.com>
2019-03-31 22:20:02 -0500

    modified functions

--- http-promise-native.js
+++ http-promise-native.js
@@ -55,3 +55,61 @@ module.exports.httpPost = (uri, headers, data)=>{
         request.end();
     });
 };
+
+
+function httpGet(uri, headers){
+
+    let options = {
+        headers: headers,
+        method: 'GET',
+    };
+    // return new pending promise
+    return new Promise((resolve, reject) => {
+        let lib = uri.startsWith('https') ? https: http;
+        let request = lib.get(uri, options, (response) => {
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                reject(new Error('error status code: ' + response.statusCode + "|" + JSON.stringify(response.body)));
+            }
+
+            let body = [];
+
+            // on every content chunk, push it to the data array
+            response.on('data', (chunk) => body.push(chunk));
+            response.on('end', () => resolve(body.join('')));
+        });
+        request.on('error', (err) => reject(err));
+        request.end();
+    });
+}
+
+
+
+function httpPost(uri, headers, data){
+
+    let options = {
+        headers: headers,
+        method: 'POST'
+    };
+
+    // return new pending promise
+    return new Promise((resolve, reject) => {
+        let lib = uri.startsWith('https') ? https: http;
+        let request = lib.request(uri, options, (response) => {
+            if (response.statusCode < 200 || response.statusCode > 299) {
+                reject(new Error('error status code: ' + response.statusCode + "|" + JSON.stringify(response.body)));
+            }
+
+            let body = [];
+
+            // on every content chunk, push it to the data array
+            response.on('data', (chunk) => body.push(chunk));
+            response.on('end', () => resolve(body.join('')));
+        });
+
+        request.on('error', (err) => reject(err));
+        if(data){
+            request.write(data);
+        }
+        request.end();
+    });
+}
